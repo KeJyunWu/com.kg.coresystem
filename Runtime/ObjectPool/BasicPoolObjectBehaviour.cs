67a0378201ce4cdf063b129dea5395fe99756e3b
kejyunwu <kejyunwu@gmail.com>
2020-12-18 16:01:29 +0800

    Object Pool Basic State

---
+++ Runtime/ObjectPool/BasicPoolObjectBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BasicPoolObjectBehaviour : MonoBehaviour, IPoolObject
+{
+    PoolProduct m_product;
+    public UnityEvent m_initEvent;
+    public UnityEvent m_recoverEvent;
+    public UnityEvent m_reuseEvent;
+
+    public PoolProduct GetPorductType()
+    {
+        return m_product;
+    }
+
+    public void Init(PoolProduct _product)
+    {
+        m_product = _product;
+        m_initEvent?.Invoke();
+    }
+
+    public void RecoverBehaviour()
+    {
+        m_recoverEvent?.Invoke();
+    }
+
+    public void ReuseBehaviour()
+    {
+        m_reuseEvent?.Invoke();
+    }
+}
