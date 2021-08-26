ae0e6a1e11d2a3a52f64fb9220fdc113f49d365a
kejyunwu <kejyunwu@gmail.com>
2021-08-26 18:11:59 +0800

    Added new feature - Keyboard trigger spawn

--- Runtime/ObjectPool/PoolProductRandomSpawner.cs
+++ Runtime/ObjectPool/PoolProductRandomSpawner.cs
@@ -1,22 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PoolProductRandomSpawner : MonoBehaviour
 {
     public Vector3 m_center;
     public Vector3 m_size;
+
+    [Header("Init Spawn")]
     public int m_count;
-    public PoolProduct m_product = PoolProduct.Product1;
+    public PoolProduct m_InitProduct = PoolProduct.Product1;
+
+    [Header("Trigger Spawn")]
+    public PoolProduct m_TriggerProduct = PoolProduct.Product1;
+    public KeyCode m_triggerKey = KeyCode.Space;
+
+
+    void ProductSpawn(PoolProduct _p)
+    {
+        GameObject _obj = ObjectPoolManager.instance.ObjectReuse(_p);
+        _obj.transform.position = CommonFunction.RandomPointInsideBox(m_center, m_size);
+        _obj.transform.parent = this.transform;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         for(var i=0; i< m_count; i++)
         {
-            GameObject _obj = ObjectPoolManager.instance.ObjectReuse(m_product);
-            _obj.transform.position = CommonFunction.RandomPointInsideBox(m_center, m_size);
-            _obj.transform.parent = this.transform;
+            ProductSpawn(m_InitProduct);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(m_triggerKey))
+        {
+            ProductSpawn(m_TriggerProduct);
         }
     }
 
