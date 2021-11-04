5ceb1b40033b9cd7e25d43b05e1cff5ba1b20cee
kejyunwu <kejyunwu@gmail.com>
2021-11-04 13:47:52 +0800

    New feature: object of pool can recover self automatically.

--- Runtime/ObjectPool/BasicPoolObjectBehaviour.cs
+++ Runtime/ObjectPool/BasicPoolObjectBehaviour.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Sirenix.OdinInspector;
 
 public class BasicPoolObjectBehaviour : MonoBehaviour, IPoolObject
 {
@@ -10,7 +11,12 @@ public class BasicPoolObjectBehaviour : MonoBehaviour, IPoolObject
     public UnityEvent m_recoverEvent;
     public UnityEvent m_reuseEvent;
 
-    public PoolProduct GetPorductType()
+	[Space]
+	public bool m_autoRecoverSelf = false;
+	[ShowIf( "m_autoRecoverSelf" ), Indent]
+	public float m_life = 3f;
+
+	public PoolProduct GetPorductType()
     {
         return m_product;
     }
@@ -29,5 +35,13 @@ public class BasicPoolObjectBehaviour : MonoBehaviour, IPoolObject
     public void ReuseBehaviour()
     {
         m_reuseEvent?.Invoke();
+		if ( m_autoRecoverSelf )
+			StartCoroutine( AutoRecover ());
     }
+
+	public IEnumerator AutoRecover()
+	{
+		yield return new WaitForSeconds( m_life );
+		ObjectPoolManager.instance.ObjectRecover( m_product, gameObject);
+	}
 }
