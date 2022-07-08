3cf479842cc4ed665c71472557c9e46b783b5543
kejyunwu <kejyunwu@gmail.com>
2022-07-08 21:24:22 +0800

    .

--- Runtime/TextureAnimation/TextureAnimation.cs
+++ Runtime/TextureAnimation/TextureAnimation.cs
@@ -2,36 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Sirenix.OdinInspector;
 
 public class TextureAnimation : MonoBehaviour
 {
     public List<Texture> m_textures = new List<Texture>();
     public float m_clipLength = 15;
-    public System.Action OnFinish;
-    public RawImage m_img;
+
+    [SerializeField]
+    public UnityEngine.Events.UnityEvent OnFinish;
+
+    bool m_playing = false;
+    float m_timer = 0;
 
     [SerializeField,ReadOnly]
     Texture m_currentFrame;
+    public Texture CurrentFrame { get { return m_currentFrame; } }
 
-    /*  // Use this for initialization
-      public void Play()
+    [Button]
+    public void Reset()
+    {
+        m_timer = 0;
+    }
+
+    [Button]
+    public void Play()
       {
-          m_animator.SetBool("Play", true);
+        m_playing = true;
       }
 
-      public void Stop()
+    [Button]
+    public void Stop()
       {
-          m_animator.SetBool("Play", false);
-      }*/
+        m_playing = false;
+        Reset();
+      }
 
     private void Update()
     {
-        m_img.texture = m_currentFrame = m_textures[(int)(((Time.time % 15) / m_clipLength) * m_textures.Count)];
-    }
+        if (m_playing)
+        {
+            m_timer += Time.deltaTime;
+            if(m_timer>= m_clipLength)
+            {
+                m_playing = false;
+                OnFinish?.Invoke();
+            }
+        }
 
-   /* void FrameEnd()
-    {
-        Stop();
-        OnFinish.Invoke();
-    }*/
+        m_currentFrame = m_textures[(int)((m_timer  / m_clipLength) * (m_textures.Count-1))];
+    }
 }
