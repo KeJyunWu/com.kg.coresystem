b02400b7b1bcf144c79e0bdb6406276a50f53e49
kejyunwu <kejyunwu@gmail.com>
2022-08-30 02:34:04 +0800

    .

--- Runtime/ObjectPool/PoolProductRandomSpawner.cs
+++ Runtime/ObjectPool/PoolProductRandomSpawner.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class PoolProductRandomSpawner : MonoBehaviour
 {
     public Vector3 m_center;
     public Vector3 m_size;
+    public Vector2 m_randomSizeRange;
 
-    [Header("Init Spawn")]
+    [Title("Automatic Spawn")]
     public int m_count;
+    [LabelText("Product")]
     public PoolProduct m_InitProduct = PoolProduct.Product1;
 
-    [Header("Trigger Spawn")]
-    public PoolProduct m_TriggerProduct = PoolProduct.Product1;
+    [Title("Manual Spawn")]
     public KeyCode m_triggerKey = KeyCode.Space;
+    [LabelText("Product")]
+    public PoolProduct m_TriggerProduct = PoolProduct.Product1;
 
 
     void ProductSpawn(PoolProduct _p)
     {
         GameObject _obj = ObjectPoolManager.instance.ObjectReuse(_p);
         _obj.transform.position = CommonFunction.RandomPointInsideBox(m_center, m_size);
+        _obj.transform.localScale = Vector3.one * Mathf.Lerp(m_randomSizeRange.x, m_randomSizeRange.y, Random.value);
         _obj.transform.parent = this.transform;
     }
 
